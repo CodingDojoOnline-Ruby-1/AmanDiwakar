902a8a1369b42f0f67384a9e00d78499fdf611bc
AmanDiwakar <aman.diwakar@gmail.com>
2017-03-01 10:11:00 -0800

    Ninja_Gold

---
+++ ninja_gold/app/controllers/rpg_controller.rb
@@ -0,0 +1,42 @@
+class RpgController < ApplicationController
+
+  def index
+      puts @gold
+  end
+  def process_gold
+      @location = params[:location]
+      @gold = 0
+      if @location == 'farm'
+          puts "You clicked farm!"
+          @loot = rand 10..20
+          @gold += @loot
+          puts "entered a #{@location} and earned #{@gold} gold"
+      elsif @location == 'cave'
+          @loot = rand 5..10
+          @gold += @loot
+          puts "entered a #{@location} and earned #{@gold} gold"
+      elsif @location == 'house'
+          @loot = rand 2..5
+          @gold += @loot
+          puts "entered a #{@location} and earned #{@gold} gold"
+      elsif @location == 'casino'
+          @loot = rand -50..50
+          if @loot == -1..-50
+              @gold -= @loot
+              puts "Loser!"
+          else
+              @gold += @loot
+              puts "Winner!"
+          end
+          puts "entered a #{@location} and earned #{@gold} gold"
+      end
+      puts "Processing gold!!"
+      puts params[:location]
+
+  end
+
+  def farm
+      @value = rand 10..20
+
+  end
+end
--- ninja_gold/config/routes.rb
+++ ninja_gold/config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  get 'rpgs/index'
-
-  root 'rpgs#index'
-
-  # get 'rpgs/cave'
-  #
-  # get 'rpgs/casino'
-  #
-  # get 'rpgs/house'
-
+  get 'rpg/index'
+  root 'rpg#index'
+  post 'process'=>'rpg#process_gold'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
