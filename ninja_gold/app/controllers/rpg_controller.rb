ca618bc2f920574eccbb1cd11901e55940ccc5ab
AmanDiwakar <aman.diwakar@gmail.com>
2017-03-02 06:57:07 -0800

    Messages show on screen

--- ninja_gold/app/controllers/rpg_controller.rb
+++ ninja_gold/app/controllers/rpg_controller.rb
@@ -2,35 +2,34 @@ class RpgController < ApplicationController
 
   def index
       params[:gold_count]
+      session
   end
   def process_gold
       session[:message] = []
-      message = session[:message]
       gold_amount = session[:gold_count]
       if session[:gold_count].nil? == false
           location = params[:location]
         if location == 'farm'
             session[:gold_count] += farm
-            message << "you found #{farm} golds at #{location}"
+            message = "you found #{farm} golds at #{location}"
+            session[:message] << message
         elsif
             location == 'cave'
             session[:gold_count] += cave
-            message << "you found #{cave} golds at #{location}"
+            session[:message] << "you found #{cave} golds at #{location}"
         elsif
             location == 'house'
             session[:gold_count] += house
-            message << "you found #{house} golds at #{location}"
+            session[:message] << "you found #{house} golds at #{location}"
         elsif
            location == 'casino'
            session[:gold_count] += casino
            puts '/'*5
            puts session
-           message << "you found #{casino} golds at #{location}"
-          end
+           session[:message] << "you found #{casino} golds at #{location}"
+        end
       else
           session[:gold_count] = 0
-
-
       end
      redirect_to :root
   end
