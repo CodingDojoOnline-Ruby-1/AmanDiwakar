092cba30a6389e03ef7a62f820f9032e5d64de40
AmanDiwakar <aman.diwakar@gmail.com>
2017-03-01 22:39:12 -0800

    Ninja Gold

--- ninja_gold/app/controllers/rpg_controller.rb
+++ ninja_gold/app/controllers/rpg_controller.rb
@@ -1,43 +1,54 @@
 class RpgController < ApplicationController
 
   def index
-      puts @gold.inspect
+      params[:gold_count]
   end
   def process_gold
-      @location = params[:location]
-      @gold = 0
-      if @location == 'farm'
-          puts "You clicked farm!"
-          @loot = rand 10..20
-          @gold += @loot
-          puts "entered a #{@location} and earned #{@gold} gold"
-      elsif @location == 'cave'
-          @loot = rand 5..10
-          @gold += @loot
-          puts "entered a #{@location} and earned #{@gold} gold"
-      elsif @location == 'house'
-          @loot = rand 2..5
-          @gold += @loot
-          puts "entered a #{@location} and earned #{@gold} gold"
-      elsif @location == 'casino'
-          @loot = rand -50..50
-          if @loot == -1..-50
-              @gold -= @loot
-              puts "Loser!"
-          else
-              @gold += @loot
-              puts "Winner!"
+      session[:message] = []
+      message = session[:message]
+      gold_amount = session[:gold_count]
+      if session[:gold_count].nil? == false
+          location = params[:location]
+        if location == 'farm'
+            session[:gold_count] += farm
+            message << "you found #{farm} golds at #{location}"
+        elsif
+            location == 'cave'
+            session[:gold_count] += cave
+            message << "you found #{cave} golds at #{location}"
+        elsif
+            location == 'house'
+            session[:gold_count] += house
+            message << "you found #{house} golds at #{location}"
+        elsif
+           location == 'casino'
+           session[:gold_count] += casino
+           puts '/'*5
+           puts session
+           message << "you found #{casino} golds at #{location}"
           end
-          puts "entered a #{@location} and earned #{@gold} gold"
+      else
+          session[:gold_count] = 0
+
+
       end
-      puts "Processing gold!!"
-      puts params[:location]
-      redirect_to :root
+     redirect_to :root
   end
 
   def farm
-      @value = rand 10..20
+      @farm = rand 10..20
+  end
 
+  def cave
+      @cave = rand 5..10
+  end
+
+  def house
+      @house = rand 2..5
+  end
+
+  def casino
+      @casino = rand -50..50
   end
 
 end
