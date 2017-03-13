11c4eedd6bb2ab29519873e968f4765dc0110510
AmanDiwakar <aman.diwakar@gmail.com>
2017-03-13 15:28:24 -0700

    working on dashboard

--- dojo_dashboard/app/controllers/Dojos_Controller.rb
+++ dojo_dashboard/app/controllers/Dojos_Controller.rb
@@ -2,4 +2,10 @@ class DojosController < ApplicationController
     def index
         @dojos = Dojo.all
     end
+    def new
+        @dojo = Dojo.new
+    end
+    def create
+        @dojo = Dojo.new(dojo_params)
+    end
 end
